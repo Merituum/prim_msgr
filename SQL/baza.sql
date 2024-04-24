0c62e16234beddecfc65b8fce4d1d618c3052778
unknown <bartolopoland@gmail.com>
2024-04-24 22:55:13 +0100

    Poprawiona baza - sql

--- SQL/baza.sql
+++ SQL/baza.sql
@@ -4,12 +4,13 @@ CREATE TABLE Users (
     Haslo VARCHAR(100) NOT NULL,
     PytaniePomocnicze VARCHAR(255) NOT NULL,
     OdpowiedzNaPytanie VARCHAR(255) NOT NULL
-    PRIMARY KEY (ID)
-
 );
+
 CREATE TABLE Wiadomosci (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     ID_wysylajacego INT NOT NULL,
-    ID_odbierajacego INT NOT NULL
--- test
+    ID_odbierajacego INT NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    FOREIGN KEY (ID_wysylajacego) REFERENCES Users(ID),
+    FOREIGN KEY (ID_odbierajacego) REFERENCES Users(ID)
 );
