fcd6bfa88b64c61f02d9ef84342dbd7811690212
Bartek Siwik <barteksiwik@MacBook-Pro-Bartek.local>
2024-05-24 18:29:01 +0200

    backend

--- login.php
+++ login.php
@@ -12,10 +12,54 @@
             <label for="login_form">Login: </label><br>
             <input type="login" id="login_form" name="login"><br>
             <label for="password_form">Hasło: </label><br>
-            <input type="password" id="password_form" name="password">
-    
+            <input type="password" id="password_form" name="password"><br>
+            <input type="submit" value="Zaloguj się" id="login_butt">
+            <!-- <button  -->
+            <!-- <input type="submit" value="Zarejestruj się"> -->
             <?php 
-            echo "hello world!"
+            // echo "hello world!";
+            
+            
+            // $db_name = "localhost";
+            // // $db_host="root";
+            // $db_user= "root";
+            // $db_pass="";    
+            // $db_name_db="prim_msgr";
+            // $conn = new mysqli($db_name, $db_user, $db_pass, $db_name_db);
+            // if ($conn->connect_error) {
+            //     die("nie dziala". $conn->connect_error);
+            //     // echo "nie dziala";
+            // }
+            // echo "</br>polaczone</br>";
+            login();
+        
+        function login (){
+            // print("testtesttest");
+            
+            $login_username = $_POST["login"];
+            // print($login_username);
+            $password_username = $_POST["password"];
+            // print($password_username);
+            $db_name = "localhost";
+            // $db_host="root";
+            $db_user= "root";
+            $db_pass="";    
+            $db_name_db="prim_msgr";
+            $conn = new mysqli($db_name, $db_user, $db_pass, $db_name_db);
+            if ($conn->connect_error) {
+                die("nie dziala". $conn->connect_error);
+                // echo "nie dziala";
+            }
+            else {
+                print("</br>dziala</br>");
+            }
+            $query_check = "SELECT * FROM users WHERE 'Login' = {$login_username} AND 'Haslo' = {$password_username}";
+            $result_check = mysqli_query($conn,$query_check);
+            print($result_check);
+            if ($result_check) {
+                print("Zalogowano");
+        }
+    }
             ?>
         </form>
     </div>    
