92f8f43210443a90f4909862c0603f86163d2a04
Merituum <bartolopoland@gmail.com>
2024-05-24 21:31:56 +0200

    Backend logowanie + rejestracja

--- login.php
+++ login.php
@@ -8,60 +8,102 @@
 </head>
 <body>
     <div class="login_window">
-         <form method='post'>
+        <form method='post'>
             <label for="login_form">Login: </label><br>
-            <input type="login" id="login_form" name="login"><br>
+            <input type="text" id="login_form" name="login"><br> <!-- changed type from "login" to "text" -->
             <label for="password_form">Hasło: </label><br>
             <input type="password" id="password_form" name="password"><br>
             <input type="submit" value="Zaloguj się" id="login_butt">
             <!-- <button  -->
-            <!-- <input type="submit" value="Zarejestruj się"> -->
+            <input type="submit" value="Zarejestruj się" name="register">
             <?php 
-            // echo "hello world!";
-            
-            
-            // $db_name = "localhost";
-            // // $db_host="root";
-            // $db_user= "root";
-            // $db_pass="";    
-            // $db_name_db="prim_msgr";
-            // $conn = new mysqli($db_name, $db_user, $db_pass, $db_name_db);
-            // if ($conn->connect_error) {
-            //     die("nie dziala". $conn->connect_error);
-            //     // echo "nie dziala";
-            // }
-            // echo "</br>polaczone</br>";
-            login();
+                if(isset($_POST["register"])){
+                    register(); 
+                }
+                else if(isset($_POST["login"])){
+                    login();
+                }
+            ?>
+
+        </form>
+    </div>
+
+    <?php
+    function register() {
+        $login_register = $_POST["login"];
+        $password_register = $_POST["password"];
+        // Assuming you have additional fields for registration like "question" and "answer"
+        $question_question = $_POST["question"]; 
+        $question_answer = $_POST["answer"];
         
-        function login (){
-            // print("testtesttest");
-            
-            $login_username = $_POST["login"];
-            // print($login_username);
-            $password_username = $_POST["password"];
-            // print($password_username);
-            $db_name = "localhost";
-            // $db_host="root";
-            $db_user= "root";
-            $db_pass="";    
-            $db_name_db="prim_msgr";
-            $conn = new mysqli($db_name, $db_user, $db_pass, $db_name_db);
-            if ($conn->connect_error) {
-                die("nie dziala". $conn->connect_error);
-                // echo "nie dziala";
+        $db_name = "localhost";
+        $db_user = "root";
+        $db_pass = "";    
+        $db_name_db = "prim_msgr";
+        
+        $conn = new mysqli($db_name, $db_user, $db_pass, $db_name_db);
+        if ($conn->connect_error) {
+            die("Nie można połączyć się z bazą danych: " . $conn->connect_error);
+        }
+
+        // Zabezpiecz dane przed SQL Injection
+        $login_register = mysqli_real_escape_string($conn, $login_register);
+        $password_register = mysqli_real_escape_string($conn, $password_register);
+        $question = mysqli_real_escape_string($conn, $question_question);
+        $answer = mysqli_real_escape_string($conn, $question_answer);
+
+        // Sprawdzenie czy użytkownik o podanym loginie już istnieje
+        $query_check = "SELECT * FROM Users WHERE Login = '$login_register'";
+        $result_check = mysqli_query($conn, $query_check);
+        if ($result_check && mysqli_num_rows($result_check) > 0) {
+            echo "Taki użytkownik już istnieje";
+        }
+        else {
+            $query_register = "INSERT INTO Users (Login, Haslo, PytaniePomocnicze, OdpowiedzNaPytanie) 
+                    VALUES ('$login_register', '$password_register', '$question', '$answer')";
+            $result_register = mysqli_query($conn, $query_register);
+            if ($result_register) {
+                session_start();
+                $_SESSION['login'] = $login_register;
+                header("Location: index.php");
+                exit();
             }
             else {
-                print("</br>dziala</br>");
+                echo "Błąd!";
             }
-            $query_check = "SELECT * FROM users WHERE 'Login' = {$login_username} AND 'Haslo' = {$password_username}";
-            $result_check = mysqli_query($conn,$query_check);
-            print($result_check);
-            if ($result_check) {
-                print("Zalogowano");
         }
+        mysqli_close($conn);
     }
-            ?>
-        </form>
-    </div>    
+
+    function login() {        
+        $login_username = $_POST["login"];
+        $password_username = $_POST["password"];
+        
+        $db_name = "localhost";
+        $db_user = "root";
+        $db_pass = "";    
+        $db_name_db = "prim_msgr";
+        
+        $conn = new mysqli($db_name, $db_user, $db_pass, $db_name_db);
+        if ($conn->connect_error) {
+            die("Nie można połączyć się z bazą danych: " . $conn->connect_error);
+        }
+        
+        // Zabezpiecz login i hasło przed SQL Injection
+        $login_username = mysqli_real_escape_string($conn, $login_username);
+        $password_username = mysqli_real_escape_string($conn, $password_username);
+
+        $query_check = "SELECT * FROM Users WHERE Login = '$login_username' AND Haslo = '$password_username'";
+        $result_check = mysqli_query($conn, $query_check);
+
+        if ($result_check && mysqli_num_rows($result_check) > 0) {
+            echo "Zalogowano pomyślnie.";
+        } else {
+            echo "Nieprawidłowy login lub hasło.";
+        }
+        mysqli_close($conn);
+    }
+    ?>
+
 </body>
 </html>
